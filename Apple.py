f490798bf2c4fd625230817a78ac5ab5c4bb6147
cassandrah62 <cassandra9hopkins@gmail.com>
2021-01-31 09:53:45 -0500

    Update Apple.py

--- Apple.py
+++ Apple.py
@@ -1,6 +1,7 @@
 def shopping(): #shopping function - maia
 
-     print("working on this")
+
+     print("It is important to buy from places that value the environment.")
 
 
 def tips():
@@ -26,12 +27,28 @@ def donating():
 def main():
 
     help = "!help"
+    
     a = input("Hello! I'm EcoPal, your guide in living a more sustainable lifestyle. type !help for a list of commands \n")
 
-    if a == help: 
+    if a == "!help": 
 
-        print("!shopping - sustainable places to shop \n !tips - everyday ways to be sustainable \n !vehicles - eco friendly vehicles and alternatives \n !volunteering - volunteering oppurtunities \n !donating - where to donate \n  ") #list of commands for the user to say -maia
-    
+        #list of commands for the user to say -maia
+
+        b = input("!shopping - sustainable places to shop \n !tips - everyday ways to be sustainable \n !vehicles - eco friendly vehicles and alternatives \n !volunteering - volunteering oppurtunities \n !donating - where to donate \n  ")
+
+        # figure out what they selected from the menu
+        if b == "!shopping":
+            shopping()
+        elif b == "!tips":
+            tips()
+        elif b == "!vehicles":
+            vehicles()
+        elif b == "!volunteering":
+            volunteering()  
+        elif b == "!donating":
+            donating()
+        else:
+            print ("Please enter a valid response")  
     
 
     else:
@@ -40,4 +57,4 @@ def main():
 
     
 
-
+main()
