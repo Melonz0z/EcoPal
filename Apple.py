3282f766fd4fd6b96b2f78e96c5d57e31e651f59
Melonz0z <Maiabot13@gmail.com>
2021-01-30 22:57:26 -0500

    edit 2

--- Apple.py
+++ Apple.py
@@ -1,14 +1,14 @@
 help = "!help"
 a = input("Hello! I'm EcoPal, your guide in living a more sustainable lifestyle. type !help for a list of commands \n")
 
-if a == help:
+if a == help: 
 
-    print("commands")
+    print("!shopping - sustainable places to shop \n !tips - everyday ways to be sustainable \n !vehicles - eco friendly vehicles and alternatives \n !volunteering - volunteering oppurtunities \n !donating - where to donate \n  ") #list of commands for the user to say -maia
     
     
 
 else:
 
-    print("error")
+    print("please type !help to get started!") #reminds user to type !help -maia
 
     
