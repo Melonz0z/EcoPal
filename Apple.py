8bfff30141ab3e8d19d66249cfe05b00131a1abb
Melonz0z <Maiabot13@gmail.com>
2021-01-30 22:37:52 -0500

    edit

--- Apple.py
+++ Apple.py
@@ -1,1 +1,14 @@
-print ("Hello world")
+help = "!help"
+a = input("Hello! I'm EcoPal, your guide in living a more sustainable lifestyle. type !help for a list of commands \n")
+
+if a == help:
+
+    print("commands")
+    
+    
+
+else:
+
+    print("error")
+
+    
