51a6e66b7727500c844c6b692e6e8fbc7af48293
Melonz0z <Maiabot13@gmail.com>
2021-01-31 00:25:24 -0500

    edit 4

--- Apple.py
+++ Apple.py
@@ -1,38 +1,43 @@
-help = "!help"
-a = input("Hello! I'm EcoPal, your guide in living a more sustainable lifestyle. type !help for a list of commands \n")
+def shopping(): #shopping function - maia
 
-if a == help: 
+     print("working on this")
 
-    print("!shopping - sustainable places to shop \n !tips - everyday ways to be sustainable \n !vehicles - eco friendly vehicles and alternatives \n !volunteering - volunteering oppurtunities \n !donating - where to donate \n  ") #list of commands for the user to say -maia
+
+def tips():
+
+    print("working on this")
+
+    
+def vehicles():
+
+    print("working on this")
+
+
+def volunteering():
+
+    print("working on this")
+
+
+def donating():
+
+    print("working on this")
+
+
+def main():
+
+    help = "!help"
+    a = input("Hello! I'm EcoPal, your guide in living a more sustainable lifestyle. type !help for a list of commands \n")
+
+    if a == help: 
+
+        print("!shopping - sustainable places to shop \n !tips - everyday ways to be sustainable \n !vehicles - eco friendly vehicles and alternatives \n !volunteering - volunteering oppurtunities \n !donating - where to donate \n  ") #list of commands for the user to say -maia
     
     
 
-else:
+    else:
 
-    print("please type !help to get started!") #reminds user to type !help -maia
-
-    
-    def shopping(): #shopping function - maia
-
-        print("working on this")
-
-
-    def tips():
-
-        print("working on this")
+        print("please type !help to get started!") #reminds user to type !help -maia
 
     
-    def vehicles():
 
-        print("working on this")
-
-
-    def volunteering():
-
-        print("working on this")
-
-
-    def donating():
-
-        print("working on this")
 
